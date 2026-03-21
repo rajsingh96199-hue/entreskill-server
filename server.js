b49029508237071e50571f5447a46f729f051482
rajsingh96199-hue <rajsingh96199.in>
2026-03-21 21:15:22 +0530

    Update CORS for production

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ connectDB()
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://entreskill-client.vercel.app'
+  ],
   credentials: true
 }))
 app.use(express.json())
